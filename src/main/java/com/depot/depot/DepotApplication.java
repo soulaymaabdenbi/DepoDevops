52f381de8cf522c6e15f6a087909ef1b226425e8
soulayma abdenbi <soulayma.abdenbi@esprit.tn>
2024-02-23 00:07:06 +0100

    First commit of the project Depo

---
+++ src/main/java/com/depot/depot/DepotApplication.java
@@ -0,0 +1,15 @@
+package com.depot.depot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepotApplication.class, args);
+    }
+
+
+}
+
